49debd5c7600cbb9411baa1f8d8603026e883223
sai <sai@gmail.com>
2023-08-03 01:49:23 +0530

    complete weather app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "af12e6820emsh30a1da2209e481ep149bd6jsn4d82e6e7d6ec",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4975e2dc2216d80ddb62099d9b127cfc"; // enter your key from openweather API
